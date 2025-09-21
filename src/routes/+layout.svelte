af7fa5ba3251305b893f17978d8f3ca325b5c561
Akaalroop Singh <akaal@akaalroop.com>
2025-09-21 20:19:46 +0100

    Added page title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,7 @@
 	<link rel="shortcut icon" href="/favicons/favicon.ico" />
 	<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
 	<link rel="manifest" href="/favicons/site.webmanifest" />
+	<title>WWF Support</title>
 </svelte:head>
 
 <div id="bg">
