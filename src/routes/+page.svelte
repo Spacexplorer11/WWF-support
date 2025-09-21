82caef826d30587ab5fc8400cae368ef936d7fd1
Akaalroop Singh <akaal@akaalroop.com>
2025-09-21 19:56:37 +0100

    Updated the text to use the colour palette and also added 50% white background to text on top the panda for legibility

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,9 +5,9 @@
 <div class="@container my-2 flex min-h-screen min-w-full flex-col" id="content">
 	<Typewriter mode="cascade" cursor={false}>
 		<header class="text-center">
-			<h1 class="mt-5"><span class="text-4xl text-cyan-400 md:text-5xl">Support for WWF</span></h1>
+			<h1 class="mt-5"><span class="text-4xl text-green md:text-5xl">Support for WWF</span></h1>
 			<h2 class="mt-5">
-				<span class="text-3xl text-cyan-400 md:text-4xl"
+				<span class="rounded-xl bg-white/50 px-2 text-3xl text-light-blue md:text-4xl"
 					>This website is about what the WWF does and how we can support</span
 				>
 			</h2>
@@ -19,7 +19,7 @@
 	#content {
 		background-image: url("/images/panda-bg.webp");
 		background-repeat: no-repeat;
-		background-size: 100vh 100vw;
+		background-size: 100vh auto;
 		background-attachment: fixed;
 		background-position: center;
 	}
