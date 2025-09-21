03b86e5cebc96542d1705a6c616b447cf31ae0ec
Akaalroop Singh <akaal@akaalroop.com>
2025-09-21 20:42:02 +0100

    Changed the homepage so text has a different background for more legibility

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,9 +5,9 @@
 <div class="@container my-2 flex min-h-screen min-w-full flex-col">
 	<Typewriter mode="cascade" cursor={false}>
 		<header class="text-center">
-			<h1 class="mt-5"><span class="text-4xl text-green md:text-5xl">Support for WWF</span></h1>
+			<h1 class="mt-5 text-4xl text-green md:text-5xl">Support for WWF</h1>
 			<h2 class="mt-5">
-				<span class="rounded-xl bg-white/50 px-2 text-3xl text-light-blue md:text-4xl"
+				<span class="rounded-xl bg-[#bcd8c1]/90 px-2 text-3xl text-light-blue md:text-4xl"
 					>This website is about what the WWF does and how we can support</span
 				>
 			</h2>
