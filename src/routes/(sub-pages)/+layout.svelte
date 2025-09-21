7e37eb4ed937561bccc7b49efaf206cadd15fd0d
Akaalroop Singh <akaal@akaalroop.com>
2025-09-21 21:25:19 +0100

    Added home button to sub pages (only 1 so far)

---
+++ src/routes/(sub-pages)/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { House } from "@lucide/svelte";
+	import { goto } from "$app/navigation";
+
+	let { children } = $props();
+</script>
+
+<button onclick={goto("/")} class="max-w-fit cursor-pointer">
+	<House class="mx-2 my-2" color="#007991" />
+</button>
+{@render children?.()}
