6a37a3ae59234356e65a6da7ac173743526e30ed
Akaalroop Singh <akaal@akaalroop.com>
2025-09-21 20:18:25 +0100

    Added favicon!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,27 @@
 <script>
 	import "../app.css";
-	import favicon from "$lib/assets/favicon.svg";
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" type="image/png" href="/favicons/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/svg+xml" href="/favicons/favicon.svg" />
+	<link rel="shortcut icon" href="/favicons/favicon.ico" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+	<link rel="manifest" href="/favicons/site.webmanifest" />
 </svelte:head>
 
 <div id="bg">
-{@render children?.()}
+	{@render children?.()}
 </div>
-	<style>
-		#bg {
-			background-image: url("/images/panda-bg.webp");
-			background-repeat: no-repeat;
-			background-size: 100vh auto;
-			background-attachment: fixed;
-			background-position: center;
-		}
-	</style>
+
+<style>
+	#bg {
+		background-image: url("/images/panda-bg.webp");
+		background-repeat: no-repeat;
+		background-size: 100vh auto;
+		background-attachment: fixed;
+		background-position: center;
+	}
+</style>
