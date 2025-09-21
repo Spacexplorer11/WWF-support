6612fb82abb108c1eed5d34993bf7c62332e9a85
Akaalroop Singh <akaal@akaalroop.com>
2025-09-21 19:45:07 +0100

    Ran prettier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,23 @@
 <script>
-	import Typewriter from 'svelte-typewriter';
+	import Typewriter from "svelte-typewriter";
 </script>
 
-<div class="@container flex flex-col min-h-screen my-2 min-w-full" id="content">
+<div class="@container my-2 flex min-h-screen min-w-full flex-col" id="content">
 	<Typewriter mode="cascade" cursor={false}>
-	<header class="text-center">
-		<h1 class="mt-5"><span class="text-4xl md:text-5xl text-cyan-400">Support for WWF</span></h1>
-		<h2 class="mt-5"><span class="text-3xl md:text-4xl text-cyan-400">This website is about what the WWF does and how we can support</span></h2>
-	</header>
+		<header class="text-center">
+			<h1 class="mt-5"><span class="text-4xl text-cyan-400 md:text-5xl">Support for WWF</span></h1>
+			<h2 class="mt-5">
+				<span class="text-3xl text-cyan-400 md:text-4xl"
+					>This website is about what the WWF does and how we can support</span
+				>
+			</h2>
+		</header>
 	</Typewriter>
 </div>
 
 <style>
 	#content {
-		background-image: url('/images/panda-bg.webp');
+		background-image: url("/images/panda-bg.webp");
 		background-repeat: no-repeat;
 		background-size: 100vh 100vw;
 		background-attachment: fixed;
