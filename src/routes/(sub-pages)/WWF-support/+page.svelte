c45de89ff4b2759f2cd8eac03d7308b1c769ddd8
Akaalroop Singh <akaal@akaalroop.com>
2025-09-22 23:06:01 +0100

    Added the main content of the wwf support page

--- src/routes/(sub-pages)/WWF-support/+page.svelte
+++ src/routes/(sub-pages)/WWF-support/+page.svelte
@@ -5,9 +5,56 @@
 <div class="@container my-2 flex min-h-screen min-w-full flex-col text-center" id="content">
 	<Typewriter mode="cascade" cursor={false}>
 		<header>
-			<h1 class="mt-5"><span class="text-4xl text-green md:text-5xl">How we can support the WWF</span></h1>
+			<h1 class="mt-5">
+				<span class="text-4xl text-green md:text-5xl">How we can support the WWF</span>
+			</h1>
 		</header>
 		<main class="my-2 space-y-2 text-dark-blue">
+			<p class="text-2xl md:text-3xl">
+				<span class="rounded-xl bg-[#bcd8c1]/80 px-2"
+					>We have a variety of ways to support the WWF - including donations but not just direct
+					donations.</span
+				>
+			</p>
+			<p class="text-2xl md:text-3xl">
+				<span class="rounded-xl bg-[#bcd8c1]/80 px-2"
+					>We can also donate our time to fundraise for them, and maybe even help them with their
+					work!</span
+				>
+			</p>
+			<p class="text-2xl md:text-3xl">
+				<span class="rounded-xl bg-[#bcd8c1]/80 px-2"
+					>We can campaign for change, rally local councils, and even protest against things like
+					deforestation.</span
+				>
+			</p>
+			<p class="text-2xl md:text-3xl">
+				<span class="rounded-xl bg-[#bcd8c1]/80 px-2"
+					>We have a voice and we can use it to save our planet.
+				</span>
+			</p>
+			<p class="text-2xl md:text-3xl">
+				<span class="rounded-xl bg-[#bcd8c1]/80 px-2"
+					>After all, all animals are our peers and without them we wouldn't be here either. So
+					please - take a little time and support the WWF. Or just donate. Every little helps!
+				</span>
+			</p>
+			<p class="text-2xl md:text-3xl">
+				<span class="rounded-xl bg-[#bcd8c1]/80 px-2"
+					>Donate <a
+						href="https://support.wwf.org.uk/donate-to-wwf"
+						class="text-dark-blue underline">here</a
+					>and fundraise
+					<a href="https://support.wwf.org.uk/pay-in-fundraising" class="text-dark-blue underline"
+						>here</a
+					>
+				</span>
+			</p>
+			<p class="text-2xl md:text-3xl">
+				<span class="rounded-xl bg-[#bcd8c1]/80 px-2"
+					>Anything, and everything is appreciated. Thank you!
+				</span>
+			</p>
 		</main>
 	</Typewriter>
 </div>
