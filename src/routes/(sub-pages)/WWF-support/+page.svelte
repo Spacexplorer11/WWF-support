f53f5f070ad4ada3a9ac5e6b42f1ad131787d6da
Akaalroop Singh <akaal@akaalroop.com>
2025-09-22 22:57:14 +0100

    Copied wwf-work structure and added heading to wwf-support page

---
+++ src/routes/(sub-pages)/WWF-support/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Typewriter from "svelte-typewriter";
+</script>
+
+<div class="@container my-2 flex min-h-screen min-w-full flex-col text-center" id="content">
+	<Typewriter mode="cascade" cursor={false}>
+		<header>
+			<h1 class="mt-5"><span class="text-4xl text-green md:text-5xl">How we can support the WWF</span></h1>
+		</header>
+		<main class="my-2 space-y-2 text-dark-blue">
+		</main>
+	</Typewriter>
+</div>
